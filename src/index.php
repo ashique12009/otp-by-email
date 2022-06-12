bf0a300f7a55c55e47fc699a7184e889aaa51fea
Ashique <ashique12009@gmail.com>
2022-06-12 07:18:12 +0600

    css added and process server side script added

--- src/index.php
+++ src/index.php
@@ -1,2 +1,21 @@
-<?php 
-echo "HERE we are in docker container";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Email OTP</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <div id="email-input-text-container" class="flex-container">
+        <div class="form-wrapper">
+            <label>Get OTP in E-mail</label>
+            <form action="process.php" class="form" method="post">
+                <input type="email" name="email" id="email" placeholder="E-mail Address" required>
+                <input type="submit" value="Submit">
+            </form>
+        </div>
+    </div>
+</body>
+</html>
